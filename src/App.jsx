66c91efd5231d5156f21f455240cde4a11ba50eb
Steffen <34124165+RegitGit@users.noreply.github.com>
2022-03-30 19:08:43 +0200

    global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./index.css"
+
 import Header from "./components/header/Header"
 import Navbar from "./components/navbar/Navbar"
 import About from "./components/about/About"
 import Projects from "./components/projects/Projects"
 import Footer from "./components/footer/Footer"
 import Contact from "./components/contact/Contact"
-import "./index.css"
 
 const App = () => {
   return (
