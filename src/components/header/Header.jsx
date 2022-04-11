dc5ada14a274a320404f855dd58047d15d7c721a
Steffen <34124165+RegitGit@users.noreply.github.com>
2022-04-11 18:16:09 +0200

    added typed.js and experimented with ::after cursor

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./header.css"
+import Typed from 'typed.js'
 
 const Header = () => {
+
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Hello, I'm<h1>Regit</h1><h5 class=\"text-light\">3D Developer</h5>"],
+      startDelay: 1000,
+      typeSpeed: 100,
+      showCursor: false,
+      cursorChar: "_",
+      onComplete(self) {
+
+      },
+      onBegin(self) {
+        
+      }
+    });
+    
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Regit</h1>
-        <h5 className="text-light">3D Developer</h5>
+        <span ref={el}></span>
+        {/* <h1>Regit</h1>
+        <h5 className="text-light">3D Developer</h5> */}
       </div>
     </header>
   )
