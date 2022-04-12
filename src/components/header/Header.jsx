29e4cbd8837724cfc2fa08506aef244466a57c86
Steffen <34124165+RegitGit@users.noreply.github.com>
2022-04-12 11:51:53 +0200

    Added Header animation timings

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,20 +5,39 @@ import Typed from 'typed.js'
 const Header = () => {
 
   const el = useRef(null);
+  const el2 = useRef(null);
+  const el3 = useRef(null);
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["Hello, I'm<h1>Regit</h1><h5 class=\"text-light\">3D Developer</h5>"],
+      strings: ["Hello, I'm"],
       startDelay: 1000,
       typeSpeed: 100,
       showCursor: false,
-      cursorChar: "_",
       onComplete(self) {
-
+        el.current.classList.remove("typewriter");
+        el2.current.classList.add("typewriter");
+        setTimeout(() => {
+          const typed2 = new Typed(el2.current, {
+            strings: ["Regit"],
+            startDelay: 200,
+            typeSpeed: 120,
+            showCursor: false,
+            onComplete(self) {
+              setTimeout(() => {
+              el2.current.classList.remove("typewriter");
+              el3.current.classList.add("typewriter");
+              const typed3 = new Typed(el3.current, {
+                strings: ["Unity Developer", "Web Developer", "3D Developer", "App Developer???", "Developer"],
+                startDelay: 200,
+                typeSpeed: 70,
+                showCursor: false,
+              });
+            }, 1000);
+            },
+          });
+        }, 500);
       },
-      onBegin(self) {
-        
-      }
     });
     
     return () => {
@@ -29,9 +48,9 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <span ref={el}></span>
-        {/* <h1>Regit</h1>
-        <h5 className="text-light">3D Developer</h5> */}
+        <span ref={el} className="typewriter"></span>
+        <h1 ref={el2}></h1>
+        <h5 ref={el3} className="text-light"></h5>
       </div>
     </header>
   )
