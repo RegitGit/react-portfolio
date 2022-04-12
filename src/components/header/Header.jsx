5bb03a7b04ae9b6833a629a32ffeb71ec39d86d6
Steffen <34124165+RegitGit@users.noreply.github.com>
2022-04-12 13:47:16 +0200

    Adjusted typewriter anim

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,12 +15,12 @@ const Header = () => {
       typeSpeed: 100,
       showCursor: false,
       onComplete(self) {
-        el.current.classList.remove("typewriter");
-        el2.current.classList.add("typewriter");
         setTimeout(() => {
+          el.current.classList.remove("typewriter");
+          el2.current.classList.add("typewriter");
           const typed2 = new Typed(el2.current, {
             strings: ["Regit"],
-            startDelay: 200,
+            startDelay: 60,
             typeSpeed: 120,
             showCursor: false,
             onComplete(self) {
@@ -28,29 +28,33 @@ const Header = () => {
               el2.current.classList.remove("typewriter");
               el3.current.classList.add("typewriter");
               const typed3 = new Typed(el3.current, {
-                strings: ["Unity Developer", "Web Developer", "3D Developer", "App Developer???", "Developer"],
+                strings: ["Unity Developer", "Web Developer", "3D Developer", "App Developer??", "Developer."],
                 startDelay: 200,
-                typeSpeed: 70,
+                typeSpeed: 50,
+                backSpeed: 20,
+                backDelay: 250,
                 showCursor: false,
+                onComplete(self) {
+                  setTimeout(() => {
+                    el3.current.classList.remove("typewriter-blink");
+                    el3.current.classList.add("typewriter-fade-out");
+                  }, 1000);
+                }
               });
             }, 1000);
             },
           });
-        }, 500);
+        }, 700);
       },
     });
-    
-    return () => {
-      typed.destroy();
-    };
   }, []);
 
   return (
     <header>
       <div className="container header__container">
-        <span ref={el} className="typewriter"></span>
-        <h1 ref={el2}></h1>
-        <h5 ref={el3} className="text-light"></h5>
+        <span ref={el} className="typewriter typewriter-blink"></span>
+        <h1 ref={el2} className="typewriter-blink"></h1>
+        <h5 ref={el3} className="text-light typewriter-blink"></h5>
       </div>
     </header>
   )
