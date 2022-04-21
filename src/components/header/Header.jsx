c484c9d99bc0f64f2e7004925defb91e87b8c57b
Steffen <34124165+RegitGit@users.noreply.github.com>
2022-04-21 13:47:46 +0200

    navbar init

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,36 +10,36 @@ const Header = () => {
   const el3 = useRef(null);
 
   useEffect(() => {
-    const typed = new Typed(el.current, {
+    new Typed(el.current, {
       strings: ["Hello, I'm"],
       startDelay: 1000,
       typeSpeed: 100,
       showCursor: false,
-      onComplete(self) {
+      onComplete() {
         setTimeout(() => {
           el.current.classList.remove("typewriter");
           el2.current.classList.add("typewriter");
-          const typed2 = new Typed(el2.current, {
+          new Typed(el2.current, {
             strings: ["Regit"],
             startDelay: 60,
             typeSpeed: 120,
             showCursor: false,
-            onComplete(self) {
+            onComplete() {
               setTimeout(() => {
               el2.current.classList.remove("typewriter");
               el3.current.classList.add("typewriter");
-              const typed3 = new Typed(el3.current, {
+              new Typed(el3.current, {
                 strings: ["Unity Developer", "Web Developer", "3D Developer", "App Developer??", "Developer."],
                 startDelay: 200,
                 typeSpeed: 50,
                 backSpeed: 20,
                 backDelay: 250,
                 showCursor: false,
-                onComplete(self) {
+                onComplete() {
                   setTimeout(() => {
                     el3.current.classList.remove("typewriter-blink");
                     el3.current.classList.add("typewriter-fade-out");
-                  }, 1000);
+                  }, 2000);
                 }
               });
             }, 1000);
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./navbar.css"
+import {AiTwotoneHome} from 'react-icons/ai'
+import {FaUserAlt} from 'react-icons/fa'
+import {MdEmail} from 'react-icons/md'
+import {BsFillFileEarmarkCodeFill} from 'react-icons/bs'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>Navbar</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><AiTwotoneHome/></a>
+      <a href='#about' onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><FaUserAlt/></a>
+      <a href='#contact' onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><MdEmail/></a>
+      <a href='#projects' onClick={() => setActiveNav("#projects")} className={activeNav === "#projects" ? "active" : ""}><BsFillFileEarmarkCodeFill/></a>
+    </nav>
   )
 }
 
