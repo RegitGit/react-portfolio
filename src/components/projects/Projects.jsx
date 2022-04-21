2d03eab6c9e800e7fa1876d812f1213f3e3b2f44
Steffen <34124165+RegitGit@users.noreply.github.com>
2022-04-21 16:17:02 +0200

    added projects first template, + basic functionallity of show by tag

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./projects.css"
+import { useState } from 'react'
+
+const data = [
+  {
+    id: 1,
+    image: "IMG1",
+    title: "Title",
+    tags: ["JS", "CSS"],
+    demo: "link"
+  },
+  {
+    id: 2,
+    image: "IMG2",
+    title: "Title2",
+    tags: ["Arduino", "Android Studio"],
+    demo: "link"
+  },
+  {
+    id: 3,
+    image: "IMG3",
+    title: "Title3",
+    tags: ["JS", "HTML"],
+    demo: "link"
+  },
+]
+
 
 const Projects = () => {
+  // Tag ordering
+  let currentTag = "";
+  function test(activeTag) {
+    let parents = document.getElementsByClassName("projects__item");
+    console.log(activeTag)
+    console.log(currentTag)
+    if (activeTag === "") return;
+    else if (currentTag === activeTag) {
+      activeTag = "";
+      for (let i = 0; i < parents.length; i++) {
+          let parent = parents[i];
+          parent.style.display = "block";
+        }
+      return;
+    }
+    currentTag = activeTag;
+    for (let i = 0; i < parents.length; i++) {
+      let parent = parents[i];
+      let child = parent.querySelector("." + activeTag)
+      if (!child) {
+        parent.style.display = "none";
+      }
+      else {
+        parent.style.display = "block";
+      }
+    }
+  }
+
   return (
-    <section id='projects'>Projects</section>
+    <section id='projects'>
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, tags, demo}) => {
+            return (
+              <article key={id} className='projects__item'>
+                <div className='projects__item-image'>
+                  <img src={image} alt={title}/>
+                </div>
+                <h3>{title}</h3>
+                <div className='projects__item-cta'>
+                  <a href={demo} className="btn btn-primary" target="_blank">Demo</a>
+                </div>
+                <div className='projects__tags'>
+                  <ul>
+                  {
+                    tags.map((tag, index) => {
+                      return (
+                        <li key={index} onClick={() => test("projects__" + tag.replace(/ /g, "_"))} className={"projects__" + tag.replace(/ /g, "_")}>{tag}</li>
+                      )
+                    })
+                  }
+                  </ul>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
