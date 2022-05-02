6a12aca9cbb2db90899c049b9f88f22ec3109381
Steffen <34124165+RegitGit@users.noreply.github.com>
2022-05-02 10:51:59 +0200

    Some css changes projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import "./projects.css"
+import IMG1 from "./IMG1.jpg"
+import IMG2 from "./IMG2.jpg"
+import IMG3 from "./IMG3.jpg"
+import {FiExternalLink} from "react-icons/fi"
 
 const data = [
   {
     id: 1,
-    image: "IMG1",
+    image: IMG1,
     title: "Title",
+    explanation: "Test",
     tags: ["JS", "CSS"],
     demo: "link"
   },
   {
     id: 2,
-    image: "IMG2",
+    image: IMG2,
     title: "Title2",
+    explanation: "Test",
     tags: ["Arduino", "Android Studio"],
     demo: "link"
   },
   {
     id: 3,
-    image: "IMG3",
+    image: IMG3,
     title: "Title3",
+    explanation: "Test",
     tags: ["JS", "HTML"],
     demo: "link"
   },
@@ -71,28 +78,29 @@ const Projects = () => {
 
   return (
     <section id='projects'>
-      <div className='container portfolio__container'>
+      <div className='container projects__container'>
         {
-          data.map(({id, image, title, tags, demo}) => {
+          data.map(({id, image, title, explanation, tags, demo}) => {
             return (
               <article key={id} className='projects__item'>
-                <div className='projects__item-image'>
-                  <img src={image} alt={title}/>
+                <div className='projects__item-image-container'>
+                  <img src={image} alt={title} className="projects__item-image"/>
                 </div>
-                <h3>{title}</h3>
                 <div className='projects__item-cta'>
-                  <a href={demo} className="btn btn-primary" target="_blank">Demo</a>
-                </div>
-                <div className='projects__tags'>
-                  <ul>
-                  {
-                    tags.map((tag, index) => {
-                      return (
-                        <li key={index} onClick={(e) => hideProjects("projects__" + tag.replace(/ /g, "_"), e.target)} className={"projects__tag projects__" + tag.replace(/ /g, "_")}>{tag}</li>
-                      )
-                    })
-                  }
-                  </ul>
+                  <h3>{title}</h3>
+                  <p>{explanation}</p>
+                  <a href={demo} className="btn btn-primary projects__item-link" target="_blank"><FiExternalLink size={24}/></a>
+                  <div className='projects__tags'>
+                    <ul>
+                    {
+                      tags.map((tag, index) => {
+                        return (
+                          <li key={index} onClick={(e) => hideProjects("projects__" + tag.replace(/ /g, "_"), e.target)} className={"projects__tag projects__" + tag.replace(/ /g, "_")}>{tag}</li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </div>
                 </div>
               </article>
             )
